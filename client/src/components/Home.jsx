684d4ecf6271881317824bc32a5c506ee5526da7
Muhammad Hanzala <malikhanzalaasif@gmail.com>
2025-02-10 06:22:50 -0800

    changed homeText font

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,8 +5,8 @@ import Button from './Button';
 const Home = () => {
   return (
     <section id='home' className='flex flex-col justify-center items-center' data-aos="fade-up">
-      <h1 className='homeText sm:text-5xl md:text-6xl font-semibold mb-6'><span className='homeBgChange'>EVERYTHING</span> CUSTOM</h1>
-      <h1 className='homeText sm:text-5xl md:text-6xl font-semibold mb-6'>YOU NEED IT, <span className='homeBgChange'>WE HAVE IT</span></h1>
+      <h1 className='homeText text-6xl md:text-7xl lg:text-8xl font-semibold mb-6'>EVERYTHING <span className='homeTextRed'>CUSTOM</span></h1>
+      <h1 className='homeText text-6xl md:text-7xl lg:text-8xl font-semibold mb-6'>YOU <span className='homeTextRed'>NEED</span> IT,WE <span className='homeTextRed'>HAVE</span> IT</h1>
       <Button />
     </section>
   )
